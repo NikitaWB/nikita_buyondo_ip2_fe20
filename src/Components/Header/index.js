568115dd20102ad63fcfbf6b58749cb521eb91a0
NikitaWB <70134216+NikitaWB@users.noreply.github.com>
2021-02-08 09:46:16 +0100

    Just added some styling

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import Navigation from './Navigation'
-import styled from 'styled-components'
+import React from "react";
+import Navigation from "./Navigation";
+import styled from "styled-components";
 
 const StyledHeader = styled.header`
-/* background: var(--deep-space-sparkle); */
-background: linear-gradient(135deg, #16425B 0%, #55828B 100%);
-color: white;
- 
-`
+  /* background: var(--deep-space-sparkle); */
+  background: linear-gradient(135deg, #16425b 0%, #55828b 100%);
+  color: white;
+  h1 {
+    padding: 10px 0px 0px 30px;
+  }
+`;
 
 function Header() {
-    return (
-        <StyledHeader>
-          <h1>Nikita Buyondo</h1>
-          <Navigation />
-        </StyledHeader>
-    )
+  return (
+    <StyledHeader>
+      <h1>Nikita Buyondo</h1>
+      <Navigation />
+    </StyledHeader>
+  );
 }
 
-export default Header
+export default Header;
--- src/Components/Main/Contact/index.js
+++ src/Components/Main/Contact/index.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    overflow: "hidden",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    fontFamily: "Roboto",
+  },
+  styledH1: {
+    textAlign: "center",
+  },
+}));
 
 function Contact() {
-    return (
-        <>
-            <h2>Contact</h2>
-        </>
-    )
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container alignItems="center" justify="space-around" spacing={5}>
+        <Grid item xs={12}>
+          <h1 className={classes.styledH1}>Contact</h1>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
